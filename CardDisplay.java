cf4002caeb0aee9d750f5b49fc7719c85307971e
josefdewberry <josefdewberry@yahoo.com>
2020-12-31 10:31:59 -0500

    Working on displaying cards by properties

--- CardDisplay.java
+++ CardDisplay.java
@@ -21,24 +21,39 @@ public class CardDisplay {
             System.out.println(cards.get(i).name);
         }
 
-        System.out.println("What card would you like to look at?");
-        Scanner input = new Scanner(System.in);
-
-        String name = input.nextLine();
-
-        for (int i = 0; i < cards.size(); i++) {
-            if (name.equals(cards.get(i).name)) {
-                if (cards.get(i).cardType == CardType.MONSTER) {
-                    MonsterCard mCard = (MonsterCard) cards.get(i);
-                    System.out.println("\n" + mCard.print());
-                } else if (cards.get(i).cardType == CardType.SPELL) {
-                    SpellCard sCard = (SpellCard) cards.get(i);
-                    System.out.println("\n" + sCard.print());
-                } else if (cards.get(i).cardType == CardType.TRAP) {
-                    TrapCard tCard = (TrapCard) cards.get(i);
-                    System.out.println("\n" + tCard.print());
+        System.out.println("A specific card?");
+        String specific = scanner.next();
+        if (specific.toLowerCase().equals("y") || specific.toLowerCase().equals("yes")) {
+            System.out.println("Enter card name: ");
+            String name = scanner.next();
+            for (int i = 0; i < cards.size(); i++) {
+                if (cards.get(i).name.equals(name)) {
+                    if (cards.get(i).cardType == CardType.MONSTER) {
+                        System.out.println("\n" + (MonsterCard) cards.get(i).print());
+                    } else if (cards.get(i).cardType == CardType.SPELL) {
+                        System.out.println("\n" + (SpellCard) cards.get(i).print());
+                    } else if (cards.get(i).cardType == CardType.TRAP) {
+                        System.out.println("\n" + (TrapCard) cards.get(i).print());
+                    }
                 }
-            }
+            } 
+        }   
+        System.out.println("Monster, Spell, or Trap card?");
+        Scanner scanner = new Scanner(System.in);
+
+        String type = scanner.next();
+        if (type.equals("Monster")) {
+
+        } else if (type.equals("Spell") || type.equals("Trap")) {
+            
+            System.out.println("What type of effect?");
+            String effect = scanner.next();
+            
+        } else {
+            System.out.println("Sorry, something went wrong.");
+            scanner.close();
+            System.exit(0);
         }
+        scanner.close();
     }
 }
